0f2d5da7c984fa304601254ce5758e900e3ce1ae
Donghee <luz0415@naver.com>
2024-08-09 02:26:58 +0900

    Add | Singleton 템플릿 클래스 추가

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance { get { Init(); return _instance; } }
+
+    private void Awake()
+    {
+        if (Init())
+            Destroy(gameObject);
+    }
+
+    static bool Init()
+    {
+        if (_instance == null)
+        {
+            _instance = FindObjectOfType<T>();
+            GameObject obj;
+
+            if (_instance == null)
+            {
+                obj = new GameObject { name = typeof(T).ToString() + "(MonoSingleton)" };
+                _instance = obj.AddComponent<T>();
+                DontDestroyOnLoad(obj);
+                return true;
+            }
+
+            obj = _instance.gameObject;
+            DontDestroyOnLoad(obj);
+            return false;
+        }
+
+        return true;
+    }
+}
