e9b375d75885cdad7694a57ed07b00a9d1eeb5b4
Donghee <luz0415@naver.com>
2024-08-12 04:14:23 +0900

    Add | 투사체 구현

--- Assets/Scripts/Player/PlayerOnStage/PlayerOnShootingStage.cs
+++ Assets/Scripts/Player/PlayerOnStage/PlayerOnShootingStage.cs
@@ -5,24 +5,45 @@ using UnityEngine.InputSystem;
 
 public class PlayerOnShootingStage : PlayerOnStage
 {
-    public PlayerOnShootingStage(Player player) : base(player) { }
-    public override void OnEnter()
-    {
+    private GameObject projectilePrefab;
+    private float shootTimer;
+    private float shootDelay = 0.5f;
+    private float moveSpeed = 5f;
 
+    public PlayerOnShootingStage(Player player) : base(player) 
+    {
+        projectilePrefab = Resources.Load<GameObject>("Prefabs/Projectile");
     }
 
-    public override void OnJump(InputAction.CallbackContext context)
+    public override void OnEnter()
     {
-
+        player.Rigid.gravityScale = 0;
+        shootTimer = Time.time;
     }
 
     public override void OnMove(InputAction.CallbackContext context)
     {
-
+        Vector2 value = context.ReadValue<Vector2>();
+        player.Rigid.velocity = value * moveSpeed;
     }
 
     public override void OnShoot(InputAction.CallbackContext context)
     {
+        if(context.performed)
+        {
+            if(shootTimer < Time.time)
+            {
+                shootTimer = Time.time + shootDelay;
 
+                GameObject projectile = Managers.ObjectPool.GetObject(projectilePrefab);
+                projectile.transform.position = player.transform.position;
+                projectile.GetComponent<Projectile>().Launch(Vector2.right);
+            }
+        }
+    }
+
+    public override void OnJump(InputAction.CallbackContext context)
+    {
+        return;
     }
 }
---
+++ Assets/Scripts/Player/Projectile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : Poolable
+{
+    private Rigidbody2D rigid;
+    private float moveSpeed = 10f;
+    private float lifeTime = 2f;
+
+    public void Launch(Vector2 direction)
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = direction * moveSpeed;
+        Invoke(nameof(ReturnToPool), lifeTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out IDamageable damageable))
+        {
+            damageable.TakeDamage(1);
+            ReturnToPool();
+        }
+    }
+}
