db2fe5e5b67880151f4ec77b127bfcf33f07ea56
Donghee <luz0415@naver.com>
2024-08-14 01:55:54 +0900

    Feat | Mouse Stage 플레이어 움직임 구현

--- Assets/Scripts/Player/PlayerOnStage/PlayerOnMouseStage.cs
+++ Assets/Scripts/Player/PlayerOnStage/PlayerOnMouseStage.cs
@@ -8,16 +8,21 @@ public class PlayerOnMouseStage : PlayerOnStage
     public PlayerOnMouseStage(Player player) : base(player) { }
     public override void OnEnter() 
     {
-
+        player.Actions.Mouse.Enable();
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     public override void OnUpdate()
     {
-
+        Vector2 mousePos = player.Actions.Mouse.Move.ReadValue<Vector2>();
+        mousePos.x = Mathf.Clamp(mousePos.x, 0, Screen.width);
+        mousePos.y = Mathf.Clamp(mousePos.y, 0, Screen.height);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        player.Rigid.position = worldPos;
     }
 
     public override void OnExit()
     {
-
+        player.Actions.Mouse.Disable();
     }
 }
