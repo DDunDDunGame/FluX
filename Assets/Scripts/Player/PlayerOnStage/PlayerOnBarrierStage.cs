e84273ab7b9c131d793047749a1b77bd19bc78c5
superjoung <46063485+superjoung@users.noreply.github.com>
2024-08-14 00:08:54 +0900

    V0.0.2
    player 움직임 수정

--- Assets/Scripts/Player/PlayerOnStage/PlayerOnBarrierStage.cs
+++ Assets/Scripts/Player/PlayerOnStage/PlayerOnBarrierStage.cs
@@ -9,28 +9,22 @@ public class PlayerOnBarrierStage : PlayerOnStage
     public PlayerOnBarrierStage(Player player) : base(player) { }
     public override void OnEnter()
     {
-
+        player.Rigid.angularDrag = 0;
+        player.Rigid.bodyType = RigidbodyType2D.Kinematic;
+        player.Actions.Barrier.Enable();
     }
 
     public override void OnUpdate()
     {
-
-    }
-
-    public override void OnMove(InputAction.CallbackContext context)
-    {
-        player.Rigid.angularDrag = 0;
-        Vector2 input = context.ReadValue<Vector2>();
-
+        Vector2 input = player.Actions.Barrier.Move.ReadValue<Vector2>();
         if (input != null)
         {
             player.Rigid.angularVelocity = input.x * 300;
         }
     }
 
-    public override void OnShoot(InputAction.CallbackContext context)
     public override void OnExit()
     {
-
+        player.Actions.Barrier.Disable();
     }
 }
