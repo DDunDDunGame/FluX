eb444ff0ee2b7a27f773b6d88cc1ef103892ee6c
Donghee <luz0415@naver.com>
2024-08-18 01:49:12 +0900

    Feat | Run Stage 패턴 1~3 추가

--- Assets/Scripts/Stage/BaseStage/RunStage.cs
+++ Assets/Scripts/Stage/BaseStage/RunStage.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class RunStage : BaseStage
 {
+    private Vector2 initPos = new(-7.5f, -2.5f);
+    private List<GameObject> patterns = new();
+    private IPattern pattern;
+
     public RunStage(StageController controller) : base(controller)
     {
+        //patterns.Add(Resources.Load<GameObject>("Prefabs/RunStage/Pattern_0"));
+        patterns.Add(Resources.Load<GameObject>("Prefabs/RunStage/Pattern_1"));
+        //patterns.Add(Resources.Load<GameObject>("Prefabs/RunStage/Pattern_2"));
     }
 
     public override void Initialize()
     {
         base.Initialize();
         Debug.Log("RunStage Initialize");
+        controller.Player.transform.position = initPos;
+        SetRandomPattern();
+    }
+
+    private void SetRandomPattern()
+    {
+        int randomIndex = Random.Range(0, patterns.Count);
+        GameObject patternObject = controller.CreateMap(patterns[randomIndex], Vector3.zero);
+        if (patternObject.TryGetComponent(out pattern)) { pattern.Initialize(controller.Player); };
     }
 
     public override void Update()
     {
         base.Update();
-        Debug.Log("RunStage Update");
+        pattern?.OnUpdate();
     }
 
     public override void Destroy()
     {
         base.Destroy();
         Debug.Log("RunStage Destroy");
+        pattern?.Destroy();
     }
 }
